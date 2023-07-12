3ab02375c13bdac48665e0070188c4d900e3f2ef
Haseop Lee <haservi@naver.com>
2023-07-13 00:42:15 +0900

    feat: servlet을 이용한 tomcat 설정

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -1,8 +1,15 @@
 package tobyspring.helloboot;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.http.HttpStatus;
+
+import java.io.IOException;
 
 public class HelloBootApplication {
 
@@ -11,7 +18,16 @@ public class HelloBootApplication {
         // jetty 등도 쓸 수 있음(동작확인은 필요)
 //        ServletWebServerFactory jettyServletWebServerFactory = new JettyServletWebServerFactory();
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer();
+        WebServer webServer = serverFactory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                    resp.setStatus(HttpStatus.OK.value());
+                    resp.setHeader("Content-type", "text/plain");
+                    resp.getWriter().println("Hello servlet");
+                }
+            }).addMapping("/hello");
+        });
         webServer.start();
     }
 
