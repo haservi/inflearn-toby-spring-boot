bc79f91ae376e4b4a70551fd2595a7221b10977f
Haseop Lee <haservi@naver.com>
2023-07-14 00:43:08 +0900

    refactor: logger 적용 및 코드 정리

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -1,9 +1,10 @@
 package tobyspring.helloboot;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -16,31 +17,42 @@ import java.io.IOException;
 
 public class HelloBootApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(HelloBootApplication.class);
+
     public static void main(String[] args) {
-        System.out.println("Hello Container Application");
-        // jetty 등도 쓸 수 있음(동작확인은 필요)
-//        ServletWebServerFactory jettyServletWebServerFactory = new JettyServletWebServerFactory();
+
+        logger.info("Hello Container Application");
+
+        // jetty 등도 쓸 수 있음
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
+
+            HelloController helloController = new HelloController();
             servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
                     // 인증, 보안, 다국어등의 공통 기능 등
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        String response = helloController.hello(name);
+
                         resp.setStatus(HttpStatus.OK.value());
                         resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-                        resp.getWriter().println("Hello servlet" + name);
+                        resp.getWriter().println(response);
+
                     } else if (req.getRequestURI().equals("/user")) {
-                        // TODO: 사용자 request 조건
-                        System.out.println("user");
+                        // TODO: 조건문 추가 필요
+                        logger.info("user");
+
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
                     }
                 }
             }).addMapping("/*");
         });
+
         webServer.start();
     }
 
