2f0c35aae7a62893d5c0a7395d13e82eb8512937
Haseop Lee <haservi@naver.com>
2023-07-16 23:24:59 +0900

    refactor: 코드 정리 및 사용하지 않는 조건 제거

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -8,7 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.http.HttpHeaders;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,35 +16,32 @@ import org.springframework.http.MediaType;
 import java.io.IOException;
 
 public class HelloBootApplication {
-
     private static final Logger logger = LoggerFactory.getLogger(HelloBootApplication.class);
 
     public static void main(String[] args) {
-
         logger.info("Hello Container Application");
+
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
 
         // jetty 등도 쓸 수 있음
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
 
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
 
-            HelloController helloController = new HelloController();
-            servletContext.addServlet("frontcontroller", new HttpServlet() {
+            servletContext.addServlet("hello", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
                     // 인증, 보안, 다국어등의 공통 기능 등
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
                         String name = req.getParameter("name");
 
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String response = helloController.hello(name);
 
-                        resp.setStatus(HttpStatus.OK.value());
-                        resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
                         resp.getWriter().println(response);
-
-                    } else if (req.getRequestURI().equals("/user")) {
-                        // TODO: 조건문 추가 필요
-                        logger.info("user");
 
                     } else {
                         resp.setStatus(HttpStatus.NOT_FOUND.value());
