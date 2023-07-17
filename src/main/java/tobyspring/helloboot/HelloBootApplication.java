8485db4c5a7ecfddf5c530d207fdd8f5a6aa1c5b
Haseop Lee <haservi@naver.com>
2023-07-18 00:40:38 +0900

    feat: 간단한 api 테스트 코드 추가

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -2,29 +2,16 @@ package tobyspring.helloboot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan
+@SpringBootApplication
 public class HelloBootApplication {
-
-  @Bean
-  public ServletWebServerFactory servletWebServerFactory() {
-    return new TomcatServletWebServerFactory();
-  }
-
-  @Bean
-  public DispatcherServlet dispatcherServlet() {
-    return new DispatcherServlet();
-  }
 
   private static final Logger logger = LoggerFactory.getLogger(HelloBootApplication.class);
 
   public static void main(String[] args) {
     logger.info("Hello Container Application");
-    MySpringApplication.run(HelloBootApplication.class, args);
+    SpringApplication.run(HelloBootApplication.class, args);
   }
 }
---
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -0,0 +1,30 @@
+package tobyspring.helloboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+class HelloApiTest {
+
+  @Test
+  void hello_api() {
+    // Given: http localhost:8080/hello?name=Spring
+    TestRestTemplate restTemplate = new TestRestTemplate();
+    ResponseEntity<String> response = restTemplate.getForEntity(
+        "http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+    // Then: status code 200
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    // Then: header -> text/plain
+    assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(
+        MediaType.TEXT_PLAIN_VALUE);
+    // Then: body Hello Spring
+    assertThat(response.getBody()).isEqualTo("Hello Spring");
+  }
+
+}
