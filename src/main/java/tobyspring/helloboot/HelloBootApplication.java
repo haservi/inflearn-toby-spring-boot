10e48d8ab907dd19ad3a94ab8f7090c53b214eef
Haseop Lee <haservi@naver.com>
2023-07-17 22:08:04 +0900

    refactor: 필요한 애노테이션만 남기고 코드 정리

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-@Configuration
 @ComponentScan
 public class HelloBootApplication {
 
