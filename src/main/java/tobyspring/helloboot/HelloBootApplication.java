ec79d0a0ff351b5d36fe6a8b5d9046b1e021b222
Haseop Lee <haservi@naver.com>
2023-07-17 23:11:57 +0900

    feat: dispatchServlet 관련 추가

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -5,12 +5,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @ComponentScan
 public class HelloBootApplication {
+
+  @Bean
+  public ServletWebServerFactory servletWebServerFactory() {
+    return new TomcatServletWebServerFactory();
+  }
+
+  @Bean
+  public DispatcherServlet dispatcherServlet() {
+    return new DispatcherServlet();
+  }
 
   private static final Logger logger = LoggerFactory.getLogger(HelloBootApplication.class);
 
@@ -21,7 +32,9 @@ public class HelloBootApplication {
       @Override
       protected void onRefresh() {
         super.onRefresh();
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+
+        ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
+        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
 
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
           servletContext.addServlet("dispatcherServlet",
--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,16 +1,24 @@
 package tobyspring.helloboot;
 
 import java.util.Objects;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
   private final HelloService helloService;
+  private final ApplicationContext applicationContext;
 
-  public HelloController(HelloService helloService) {
+  public HelloController(HelloService helloService, ApplicationContext applicationContext) {
     this.helloService = helloService;
+    this.applicationContext = applicationContext;
+
+    logger.info("applicationContext = " + applicationContext);
   }
 
   @GetMapping("/hello")
