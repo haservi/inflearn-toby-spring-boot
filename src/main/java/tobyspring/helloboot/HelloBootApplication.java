1f003f29b5c73ae3ac3ce033af82eaa35f238e60
Haseop Lee <haservi@naver.com>
2023-07-17 23:16:47 +0900

    refactor: run 메서드를 이용해 코드 분리

--- src/main/java/tobyspring/helloboot/HelloBootApplication.java
+++ src/main/java/tobyspring/helloboot/HelloBootApplication.java
@@ -3,11 +3,9 @@ package tobyspring.helloboot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @ComponentScan
@@ -27,26 +25,6 @@ public class HelloBootApplication {
 
   public static void main(String[] args) {
     logger.info("Hello Container Application");
-
-    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-      @Override
-      protected void onRefresh() {
-        super.onRefresh();
-
-        ServletWebServerFactory serverFactory = this.getBean(ServletWebServerFactory.class);
-        DispatcherServlet dispatcherServlet = this.getBean(DispatcherServlet.class);
-
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-          servletContext.addServlet("dispatcherServlet",
-              new DispatcherServlet(this)
-          ).addMapping("/*");
-        });
-        webServer.start();
-      }
-    };
-    applicationContext.register(HelloBootApplication.class);
-    applicationContext.refresh();
-
+    MySpringApplication.run(HelloBootApplication.class, args);
   }
-
 }
