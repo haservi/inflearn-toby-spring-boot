0a11a359d1c3632e023156f03719b869e330b2ab
Haseop Lee <haservi@naver.com>
2023-07-20 23:41:12 +0900

    feat: config 클래스 명시적으로 선언하는 방법

--- src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
+++ src/main/java/tobyspring/helloboot/MySpringBootAnnotation.java
@@ -1,16 +1,19 @@
 package tobyspring.helloboot;
 
+import config.Config;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Configuration
 @ComponentScan
+@Import(Config.class)
 public @interface MySpringBootAnnotation {
 
 }
