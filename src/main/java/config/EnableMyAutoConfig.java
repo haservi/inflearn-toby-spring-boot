afc8861b06a4d5b031a5dc957684cfcaf5086681
Haseop Lee <haservi@naver.com>
2023-07-20 23:49:03 +0900

    feat: 인프라 빈 구성 정보 분리 학습

---
+++ src/main/java/config/EnableMyAutoConfig.java
@@ -0,0 +1,16 @@
+package config;
+
+import config.autoconfig.DispatcherServletConfig;
+import config.autoconfig.TomcatWebServerConfig;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.context.annotation.Import;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+@Import({DispatcherServletConfig.class, TomcatWebServerConfig.class})
+public @interface EnableMyAutoConfig {
+
+}
--- src/main/java/config/MySpringBootAnnotation.java
+++ src/main/java/config/MySpringBootAnnotation.java
@@ -1,19 +1,17 @@
-package tobyspring.helloboot;
+package config;
 
-import config.Config;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @Configuration
 @ComponentScan
-@Import(Config.class)
+@EnableMyAutoConfig
 public @interface MySpringBootAnnotation {
 
 }
