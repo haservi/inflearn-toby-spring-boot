c782301341f08627f94ca9a67ab77d872c93b2ce
Haseop Lee <haservi@naver.com>
2023-07-30 20:55:25 +0900

    feat: @value를 이용하여 env context 설정

--- src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -1,10 +1,12 @@
 package tobyspring.config.autoconfig;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.Environment;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
@@ -12,20 +14,20 @@ import tobyspring.config.MyAutoConfiguration;
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
 public class TomcatWebServerConfig {
 
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+  @Value("${contextPath}")
+  String contextPath;
+
   @Bean("tomcatWebServerFactory")
   @ConditionalOnMissingBean
-  public ServletWebServerFactory servletWebServerFactory(Environment environment) {
+  public ServletWebServerFactory servletWebServerFactory() {
     TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-    serverFactory.setContextPath(environment.getProperty("contextPath"));
+
+    logger.info("contextPath: {}", this.contextPath);
+
+    serverFactory.setContextPath(this.contextPath);
     return serverFactory;
   }
 
-//  static class TomcatCondition implements Condition {
-//
-//    @Override
-//    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-//      return ClassUtils.isPresent("org.apache.catalina.startup.Tomcat",
-//          context.getClassLoader());
-//    }
-//  }
 }
