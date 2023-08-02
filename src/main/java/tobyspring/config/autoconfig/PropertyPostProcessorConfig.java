44b5f3e2215e9aeb73d59dc5f8ed26d1a8b7f3c5
Haseop Lee <haservi@naver.com>
2023-08-02 23:29:08 +0900

    feat: 프로퍼티 빈의 후처리기 내용 추가..

---
+++ src/main/java/tobyspring/config/autoconfig/PropertyPostProcessorConfig.java
@@ -0,0 +1,39 @@
+package tobyspring.config.autoconfig;
+
+import static org.springframework.core.annotation.AnnotationUtils.findAnnotation;
+import static org.springframework.core.annotation.AnnotationUtils.getAnnotationAttributes;
+
+import java.util.Map;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.boot.context.properties.bind.Binder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import tobyspring.config.MyAutoConfiguration;
+import tobyspring.config.MyConfigurationProperties;
+
+@MyAutoConfiguration
+public class PropertyPostProcessorConfig {
+
+  @Bean
+  BeanPostProcessor beanPostProcessor(Environment environment) {
+    return new BeanPostProcessor() {
+      @Override
+      public Object postProcessAfterInitialization(Object bean, String beanName)
+          throws BeansException {
+
+        MyConfigurationProperties annotation = findAnnotation(bean.getClass(),
+            MyConfigurationProperties.class);
+
+        if (annotation == null) {
+          return bean;
+        }
+
+        Map<String, Object> annotationAttributes = getAnnotationAttributes(annotation);
+        String prefix = (String) annotationAttributes.get("prefix");
+
+        return Binder.get(environment).bindOrCreate(prefix, bean.getClass());
+      }
+    };
+  }
+}
