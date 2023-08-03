df6322ccdd2e4185847f04c2f70a683d6c173e27
Haseop Lee <haservi@naver.com>
2023-08-03 22:14:18 +0900

    fixed: conditionMyOnClass 잘못 넣은 부분 수정

--- src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
+++ src/main/java/tobyspring/config/autoconfig/DataSourceConfig.java
@@ -4,9 +4,12 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Driver;
 import javax.sql.DataSource;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.jdbc.support.JdbcTransactionManager;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
@@ -18,6 +21,19 @@ public class DataSourceConfig {
 
   @Bean
   @ConditionalMyOnClass("com.zaxxer.hikari.HikariDataSource")
+  @ConditionalOnMissingBean
+  DataSource hikariDataSource(DataSourceProperties properties) {
+    HikariDataSource dataSource = new HikariDataSource();
+
+    dataSource.setDriverClassName(properties.getDriverClassName());
+    dataSource.setJdbcUrl(properties.getUrl());
+    dataSource.setUsername(properties.getUsername());
+    dataSource.setPassword(properties.getPassword());
+
+    return dataSource;
+  }
+
+  @Bean
   @ConditionalOnMissingBean
   DataSource dataSource(MyDataSourceProperties properties) throws ClassNotFoundException {
     SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
@@ -32,16 +48,17 @@ public class DataSourceConfig {
   }
 
   @Bean
+  @ConditionalOnSingleCandidate(DataSource.class)
   @ConditionalOnMissingBean
-  DataSource hikariDataSource(DataSourceProperties properties) {
-    HikariDataSource dataSource = new HikariDataSource();
+  JdbcTemplate jdbcTemplate(DataSource dataSource) {
+    return new JdbcTemplate();
+  }
 
-    dataSource.setDriverClassName(properties.getDriverClassName());
-    dataSource.setJdbcUrl(properties.getUrl());
-    dataSource.setUsername(properties.getUsername());
-    dataSource.setPassword(properties.getPassword());
-
-    return dataSource;
+  @Bean
+  @ConditionalOnSingleCandidate(DataSource.class)
+  @ConditionalOnMissingBean
+  JdbcTransactionManager jdbcTransactionManager(DataSource dataSource) {
+    return new JdbcTransactionManager();
   }
 
 }
